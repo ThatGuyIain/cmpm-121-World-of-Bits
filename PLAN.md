153d116b4d7e26d1bac55b0e0d1fc8b5c5a9ba18
ThatGuyIain <iklotzba@ucsc.edu>
2025-11-10 21:44:53 +0000

    - Created a loadable map using Leaflet - Added a tile generator that is consistent across reloads

--- PLAN.md
+++ PLAN.md
@@ -8,11 +8,11 @@
 
 ### D3.A: Core Mechanics
 
-[] Create a PLAN.md
-[] Load a map use Leaflet
-[] Randomly generate cell tiles on the map
-[] Make tile generation consistent across reloads
-[] Make tiles interactable within a limit from the player
-[] Create an inventory system that tracks tokens collected from tiles
-[] Display total tokens held if they player has tokens
-[] Crafting using tokens
+- [✔️] Create a PLAN.md
+- [✔️] Load a map use Leaflet
+- [✔️] Generate cell tiles on the map
+- [✔️] Make tile generation consistent across reloads
+- [] Make tiles interactable within a limit from the player
+- [] Create an inventory system holds tokens collected from tiles
+- [] Display total tokens held if they player has tokens
+- [] Crafting using tokens
---
+++ src/main.ts
@@ -0,0 +1,71 @@
+/************************************************
+ *
+ * Leaflet Map Setup
+ * Tile Generation
+ *
+ ************************************************/
+
+import leaflet from "leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+import "./_leafletWorkaround.ts";
+
+import luck from "./_luck.ts";
+
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+/*--------------------------SETTING UP LEAFLET MAP--------------------------*/
+
+const mapDiv = document.createElement("div");
+mapDiv.id = "map";
+document.body.append(mapDiv);
+
+const CLASSROOM_LATLNG = leaflet.latLng(
+  36.997936938057016,
+  -122.05703507501151,
+);
+
+const map = leaflet.map(mapDiv, {
+  center: CLASSROOM_LATLNG,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+/*--------------------------TILE GENERATION--------------------------*/
+
+function spawnCache(i: number, j: number) {
+  // Convert cell numbers into lat/lng bounds
+  const origin = CLASSROOM_LATLNG;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  // Add a rectangle to the map to represent the cache
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+}
+
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
+}
