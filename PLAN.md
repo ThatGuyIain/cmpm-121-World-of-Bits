ef912a88d15cad19d33815a5faf66bed318230b4
ThatGuyIain <iklotzba@ucsc.edu>
2025-11-17 22:50:00 +0000

    -  Added inventory system that tracks if the player has a token and it's amount - Added a Display for the current token

--- PLAN.md
+++ PLAN.md
@@ -8,13 +8,13 @@
 
 ### D3.A: Core Mechanics
 
-- [x️️] Create a PLAN.md
-- [x] Load a map use Leaflet
-- [x] Generate cell tiles on the map
-- [x] Make tile generation consistent across reloads
-- [x] Create a player marker
-- [] Make tiles interactable within a limit from the player
-- [x] Create an inventory system that can only hold a token
-- [] Display for the current token held if the player has one
-- [] System to display if the current held token can be combined with any nearby tokens
-- [] Allow player to combine held token with compatible map token
+[x️️] Create a PLAN.md
+[x] Load a map use Leaflet
+[x] Generate cell tiles on the map
+[x] Make tile generation consistent across reloads
+[x] Create a player marker
+[] Make tiles interactable within a limit from the player
+[x] Create an inventory system that can only hold a token
+[x] Display for the current token held if the player has one
+[] System to display if the current held token can be combined with any nearby tokens
+[] Allow player to combine held token with compatible map token
--- src/main.ts
+++ src/main.ts
@@ -1,110 +1,227 @@
 /************************************************
  *
+ * Data Types
+ * Constants
+ * Global Variables
  * Leaflet Map Setup
+ * Player Information
  * Tile Generation
+ * Temporary MISC
+ * Main
  *
  ************************************************/
 
-import leaflet from "leaflet";
-
+// @deno-types="npm:@types/leaflet"
+import Leaflet from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./style.css";
-
 import "./_leafletWorkaround.ts";
-
 import luck from "./_luck.ts";
+
+/*--------------------------DATA TYPES--------------------------*/
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+interface Token {
+  value: number;
+}
+
+interface CellOptions extends Leaflet.PolylineOptions {
+  token?: Token;
+  centerDist?: Point;
+}
+
+/*--------------------------CONSTANTS--------------------------*/
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
 const CACHE_SPAWN_PROBABILITY = 0.1;
+const SEEDEDNUMBER = 131;
 
-/*--------------------------SETTING UP LEAFLET MAP--------------------------*/
+/*--------------------------GLOBAL VARIABLES--------------------------*/
 
-const mapDiv = document.createElement("div");
-mapDiv.id = "map";
-document.body.append(mapDiv);
-
-const CLASSROOM_LATLNG = leaflet.latLng(
+const CLASSROOM_LATLNG = Leaflet.latLng(
   36.997936938057016,
   -122.05703507501151,
 );
 
-const map = leaflet.map(mapDiv, {
-  center: CLASSROOM_LATLNG,
-  zoom: GAMEPLAY_ZOOM_LEVEL,
-  minZoom: GAMEPLAY_ZOOM_LEVEL,
-  maxZoom: GAMEPLAY_ZOOM_LEVEL,
-  zoomControl: false,
-  scrollWheelZoom: false,
-});
+let mapDiv: HTMLDivElement;
+let map: Leaflet.Map;
+let inventoryDiv: HTMLDivElement;
 
-leaflet
-  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
+const cellMarkers = new Map<Leaflet.Rectangle, Leaflet.Marker>();
+
+/*--------------------------SETTING UP LEAFLET MAP--------------------------*/
+
+function createMap(): void {
+  mapDiv = document.createElement("div");
+  mapDiv.id = "map";
+  document.body.append(mapDiv);
+
+  map = Leaflet.map(mapDiv, {
+    center: CLASSROOM_LATLNG,
+    zoom: GAMEPLAY_ZOOM_LEVEL,
+    minZoom: GAMEPLAY_ZOOM_LEVEL,
+    maxZoom: GAMEPLAY_ZOOM_LEVEL,
+    zoomControl: false,
+    scrollWheelZoom: false,
+  });
+
+  Leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: GAMEPLAY_ZOOM_LEVEL,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  })
-  .addTo(map);
+  }).addTo(map);
+
+  playerMarker = Leaflet.marker(CLASSROOM_LATLNG);
+  playerMarker.bindTooltip("That's you!");
+  playerMarker.addTo(map);
+}
 
 /*--------------------------PLAYER INFORMATION--------------------------*/
 
-const playerMarker = leaflet.marker(CLASSROOM_LATLNG);
-playerMarker.addTo(map);
-
-const position = CLASSROOM_LATLNG;
-let token: number = 0;
-let hasToken: boolean = false;
-
-if (token) console.log("Funny temporary check work around");
-if (position) console.log("Funny temporary check work around");
+let playerMarker: Leaflet.Marker;
+let inventory: Token | null = null;
 
 /*--------------------------TILE GENERATION--------------------------*/
 
-function spawnCache(i: number, j: number) {
-  // Convert cell numbers into lat/lng bounds
-  const origin = CLASSROOM_LATLNG;
-  const bounds = leaflet.latLngBounds([
-    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
-    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
-  ]);
+function drawCells(): void {
+  const bounds = map.getBounds();
+  const south = bounds.getSouth();
+  const north = bounds.getNorth();
+  const west = bounds.getWest();
+  const east = bounds.getEast();
 
-  // Add a rectangle to the map to represent the cache
-  const rect = leaflet.rectangle(bounds);
-  rect.addTo(map);
+  for (let lat = south; lat <= north; lat += TILE_DEGREES) {
+    for (let lng = west; lng <= east; lng += TILE_DEGREES) {
+      const seed = `${lat}, ${lng}`;
+      if (luck(seed) >= CACHE_SPAWN_PROBABILITY) continue;
 
-  rect.bindPopup(() => {
-    const popUp = document.createElement("div");
-    popUp.innerHTML =
-      'Token: <span id="value">${pointValue}</span>.</div>< button id = "poke" > PRESS ME </button>';
+      const tileBounds: Leaflet.LatLngBoundsLiteral = [
+        [lat, lng],
+        [lat + TILE_DEGREES, lng + TILE_DEGREES],
+      ];
 
-    //TODO: RANDOMLY SEED TOKEN TO A SPECIFIC VALUE
-    let tokenValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+      const tokenValue = getRandomTokenValue(seed);
 
-    popUp.querySelector<HTMLButtonElement>("#poke")!.addEventListener(
-      "click",
-      () => {
-        //TODO: ADD A POSITION CHECKER
-        if (!hasToken) {
-          token = tokenValue;
-          tokenValue = 0;
-          popUp.querySelector<HTMLSpanElement>("#value")!.innerHTML = tokenValue
-            .toString();
-          hasToken = true;
-        } else {
-          //TODO: PROPER CAN'T DO THAT MESSAGE
-          console.log("Sorry- Temporary can't do that message");
-        }
-      },
-    );
-    return popUp;
-  });
-}
+      const iconMarker = createIcon(tokenValue, tileBounds);
+      const rect = createRectangle(tokenValue, tileBounds);
 
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
-      spawnCache(i, j);
+      cellMarkers.set(rect, iconMarker);
     }
   }
 }
+
+function createIcon(
+  tokenValue: number,
+  tileBoundsLiteral: Leaflet.LatLngBoundsLiteral,
+): Leaflet.Marker {
+  const tileBounds = Leaflet.latLngBounds(tileBoundsLiteral);
+  const centerDist = getDistanceFromCenter(tileBounds);
+
+  const icon = Leaflet.divIcon({
+    html: `<p>${tokenValue}</p>`,
+    className: "icon",
+    iconAnchor: [centerDist.x + 6, centerDist.y + 40],
+  });
+
+  const center = tileBounds.getCenter();
+  const iconMarker = Leaflet.marker(center, {
+    icon: icon,
+    interactive: false,
+  });
+
+  iconMarker.addTo(map);
+  return iconMarker;
+}
+
+function createRectangle(
+  tokenValue: number,
+  tileBoundsLiteral: Leaflet.LatLngBoundsLiteral,
+): Leaflet.Rectangle {
+  const tileBounds = Leaflet.latLngBounds(tileBoundsLiteral);
+
+  const rectOptions: CellOptions = {
+    token: { value: tokenValue },
+    centerDist: getDistanceFromCenter(tileBounds),
+  };
+  const rect = Leaflet.rectangle(tileBounds, rectOptions);
+
+  rect.on("click", function (e) {
+    const temp = inventory;
+    inventory = e.target.options.token;
+    e.target.options.token = temp;
+
+    updateInventoryDisplay();
+    updateCellDisplay(e.target, e.target.options.token);
+  });
+
+  rect.addTo(map);
+  return rect;
+}
+
+/*--------------------------TEMPORARY MISC--------------------------*/
+
+function getRandomTokenValue(seed: string): number {
+  const randomValue = luck(seed);
+  console.log(randomValue);
+  if (randomValue <= 0.06) return SEEDEDNUMBER;
+  if (randomValue <= 0.09) return (SEEDEDNUMBER * 2);
+  return (SEEDEDNUMBER * 4);
+}
+
+function getDistanceFromCenter(tilebounds: Leaflet.LatLngBounds): Point {
+  const centerLat = tilebounds.getNorth() - tilebounds.getCenter().lat;
+  const centerLng = tilebounds.getEast() - tilebounds.getCenter().lng;
+  return { x: centerLat, y: centerLng };
+}
+
+function updateInventoryDisplay(): void {
+  if (inventory == null) inventoryDiv.textContent = "No held tokens.";
+  else inventoryDiv.textContent = `Held token: ${inventory.value}`;
+}
+
+function updateCellDisplay(
+  rect: Leaflet.Rectangle,
+  newToken: Token | null,
+): void {
+  const marker = cellMarkers.get(rect);
+  const options = rect.options as CellOptions;
+
+  if (marker && options.centerDist) {
+    let icon: Leaflet.DivIcon;
+
+    if (newToken != null) {
+      icon = Leaflet.divIcon({
+        html: `<p>${newToken.value}</p>`,
+        className: "icon",
+        iconAnchor: [options.centerDist.x + 6, options.centerDist.y + 40],
+      });
+    } else {
+      icon = Leaflet.divIcon({
+        html: `<p> </p>`,
+        className: "icon",
+        iconAnchor: [options.centerDist.x + 6, options.centerDist.y + 40],
+      });
+    }
+
+    marker.setIcon(icon);
+  }
+}
+
+/*--------------------------MAIN--------------------------*/
+
+function main(): void {
+  createMap();
+  drawCells();
+
+  inventoryDiv = document.createElement("div");
+  inventoryDiv.id = "statusPanel";
+  inventoryDiv.textContent = "No held tokens.";
+  document.body.append(inventoryDiv);
+}
+
+main();
